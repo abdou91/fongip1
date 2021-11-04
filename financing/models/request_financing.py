b9ff62275eec91399bd67efd89d06f316af3c339
Abdou Mbar Ly <abdoumbarly@gmail.com>
2021-11-04 16:54:58 +0000

    update financing module

--- financing/models/request_financing.py
+++ financing/models/request_financing.py
@@ -8,31 +8,25 @@ RECEPTION_MODE = [
 						('dossier_electronique','Dossier électronique'),
 						('dossier_physique','Dossier physique'),
 				 ]
-class FinancingRequest(models.Models):
-	_name = 'financing.request'
-	_description = 'Request financing'
 
-	name = fields.Char()
-	number = fields.Char(string = "Numéro")
-	reception_mode = fields.Selection(
-		RECEPTION_MODE,
-		default="dossier_physique",
-		string = "Mode de réception"
-	)
-	currency_id = fields.Many2one(
-        'res.currency', 'Currency', default=lambda self: self.env.company.currency_id.id
-    )
 
+class FinancingRequest(models.Model):
+    _name = 'financing.request'
+    _description = 'Demande de financement'
+
+    name = fields.Char()
+    number = fields.Char(string = "Numéro")
+    reception_mode = fields(RECEPTION_MODE,default="dossier_physique",string = "Mode de réception")
+    currency_id = fields.Many2one('res.currency','Currency',default=lambda self: self.env.company.currency_id.id)
     project_cost = fields.Monetary(string = "Cout du projet")
     credit_requested = fields.Monetary(string = "Crédit sollicité")
     quotite = fields.Float(string = "Quotité de garantie")
     guarantee_amount = fields.Monetary(string = "Garantie éventuelle",compute = '_compute_guarantee_amount',store=True)
-    transmission_date = fields.Date(string = "Date de transmission")
+    transmission_date = fields.Date(string = "Date d'imputation à l'ingénieur")
     number_of_job = fields.Integer(string = "Nombre d'emplois")
     imputation_date = fields.Date(string = "Date d'imputation à l'ingénieur")
     transmitted_to = fields.Many2one('hr.employee',string = "Transmis à")
-    partner_id = fields.Many2one('res.partner',string = "Porteur de projet")
-    #partner_name = fields.Char(string = "")
+    partner_id =  fields.Many2one('res.partner',string = "Porteur de projet")
 
     @api.depends('credit_requested','quotite')
     def _compute_guarantee_amount(self):
