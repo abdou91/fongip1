27e1e11428a2b8ee39113f0f6118710f75ac99d3
Abdou Mbar Ly <abdoumbarly@gmail.com>
2021-11-04 17:25:33 +0000

    update financing module

--- hr_holidays_inherit/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import api_sms
-from . import hr_holidays
-from . import hr_holidays_public
-from . import res_config_settings
--- hr_pointage/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-from . import hr_absence
-from . import hr_employee
-from . import hr_horaire
-from . import hr_pointage
-from . import hr_presence
