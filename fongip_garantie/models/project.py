0bc7172ae5412f253e169ac0b393304c3201ed1c
Abdou Mbar Ly <abdoumbarly@gmail.com>
2021-11-10 17:13:56 +0000

    update garantie module

--- fongip_garantie/models/project.py
+++ fongip_garantie/models/project.py
@@ -205,7 +205,7 @@ class FongipProject(models.Model):
 		dossier = self.search([('action','=','archive'),('name','like',self.name)],limit = 1)
 		xml_id = self.env.context.get('xml_id')
 		if xml_id:
-			res = self.env['ir.actions.act_window']._for_xml_id('fongip_garantie.%s' %  xml_id)  #for_xml_id('fongip_garantie',xml_id)
+			res = self.env['ir.actions.act_window']._for_xml_id('fongip_garantie.%s' % xml_id)
 			res.update(
 						context=dict(self.env.context,active_id=dossier.id, group_by=False),
 						domain=[('id','=',dossier.id)]
@@ -343,7 +343,7 @@ class FongipProject(models.Model):
 		self.ensure_one()
 		xml_id = self.env.context.get('xml_id')
 		if xml_id:
-			res = self.env['ir.actions.act_window']._for_xml_id('fongip_garantie.%s' %  xml_id)#for_xml_id('fongip_garantie',xml_id)
+			res = self.env['ir.actions.act_window']._for_xml_id('fongip_garantie.%s' % xml_id)
 			res.update(
 						context=dict(self.env.context,default_project_id=self.id, group_by=False),
 						domain=[('project_id','=',self.id)]
